7dec6c8d28f28e38fbd540b7446ded1737ea6287
Peter B. Youssef <Peto.barkar@hotmail.com>
2022-10-25 20:09:27 +0200

    Add FileOpetaions model.

---
+++ src/model/FileOpetaions.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.ArrayList;
+
+public class FileOpetaions {
+
+    public FileOpetaions() {
+    }
+
+    public ArrayList<InvoiceHeader> readFile(){
+        return null;
+    }
+
+    public void writeFile(ArrayList<InvoiceHeader> arrayList){
+
+    }
+}
